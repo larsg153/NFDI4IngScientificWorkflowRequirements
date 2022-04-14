760682266c17ff178311c4936b37401ef708d37e
larsg <lars.griem@kit.edu>
2022-04-14 09:25:10 +0200

    fixed bug in workflow-nodes

--- simple_use_case/workflow-nodes/workflow_nodes/misc/gmsh.py
+++ simple_use_case/workflow-nodes/workflow_nodes/misc/gmsh.py
@@ -91,7 +91,9 @@ def gmsh(
     if three_dimensional_mesh:
         cmd += ["-3"]
     if set_number:
-        cmd += ["-setnumber", set_number]
+        set_number_list = set_number.split(' ')
+        cmd += ["-setnumber"]
+        cmd += set_number_list
     if geo_file:
         cmd += [geo_file]
     if save_and_exit:
